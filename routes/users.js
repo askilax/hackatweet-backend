421bf06b8090c3773b8daa8c43b96b5f7abb8469
Maxime Sbaizero <maximesbaizero@gmail.com>
2024-10-11 12:15:24 +0200

    maj des routes users

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,21 @@ const User = require('../models/users')
 
 router.post('/signup', async (req, res) => {
   // recup les infos envoyer
-  let { firstname, username, password } = req.body
+  let { firstName, userName, password } = req.body
   // vérifie si les champs sont remplis
-  if (!firstname|| !username || !password) {
+  if (!firstName || !userName || !password) {
     return res.json({ result: false, Error: 'Missing or empty fields.' })
   }
   // en minuscule
-  firstname = firstname.toLowerCase();
-  username = username.toLowerCase();
+  firstName = firstName.toLowerCase();
+  userName = userName.toLowerCase();
   try {
     // vérifie si il existe deja en DB
-    const existUser = await User.findOne({ username });
+    const existUser = await User.findOne({ userName: `@${userName}` });
     if (existUser) {
       return res.json({ result: false, Error: 'User already registered.' })
     }
-// hash le password avec argon2id (meilleurs que bcrypt)
+    // hash le password avec argon2id (meilleurs que bcrypt)
     const hashedPass = await argon2.hash(password, {
       type: argon2.argon2id,
       memoryCost: 131072,
@@ -29,10 +29,10 @@ router.post('/signup', async (req, res) => {
     // token et enregistrement de l'utilisateur 
     const token = uid2(32)
     const newUser = new User({
-      firstname,username, password: hashedPass, token,
+      firstName, userName, password: hashedPass, token,
     })
     await newUser.save();
-    res.status(200).json({ result: true, Message: `${username} registered successfully.`, token })
+    res.status(200).json({ result: true, Message: `@${userName} registered successfully.`, token, userName: `@${userName}`, firstName })
   } catch (error) {
     res.status(500).json({ result: false, message: 'Server error', error });
   }
@@ -40,26 +40,26 @@ router.post('/signup', async (req, res) => {
 
 
 router.post('/signin', async (req, res) => {
-    // recup les infos envoyer
-  let {username, password } = req.body;
-    // vérifie si les champs sont remplis
-  if (!username || !password) {
+  // recup les infos envoyer
+  let { userName, password } = req.body;
+  // vérifie si les champs sont remplis
+  if (!userName || !password) {
     return res.json({ result: false, error: 'Missing or empty fields.' })
   };
-   // en minuscule
-   username = username.toLowerCase();
+  // en minuscule
+  userName = userName.toLowerCase();
   try {
-        // vérifie si il existe deja en DB
-    const existUser = await User.findOne({ username: `@${username}` });
+    // vérifie si il existe deja en DB
+    const existUser = await User.findOne({ userName: `@${userName}` });
     if (!existUser) {
       return res.json({ result: false, error: 'User not found' })
     }
-// compare les deux mots de passe le permier est celui de la DB et le deuxieme celui reçu
+    // compare les deux mots de passe le permier est celui de la DB et le deuxieme celui reçu
     const isMatch = await argon2.verify(existUser.password, password);
     if (!isMatch) {
       return res.json({ result: false, error: 'Incorrect password.' })
     }
-    res.status(200).json({ result: true, message: `@${username} Signin successful`, token: existUser.token, firstName: existUser.firstname, userName: existUser.username })
+    res.status(200).json({ result: true, message: `@${userName} Signin successful`, token: existUser.token, firstName: existUser.firstName, userName: existUser.userName })
   } catch (error) {
     res.status(500).json({ result: false, error: 'server error', error })
   }
